203f0e4dc86ef44e754e3cd60fc3c6aa8371335d
Andres V <andres.vazquezbrennan@physics.ox.ac.uk>
2025-02-11 22:28:29 +0000

    pmuse: Make Waveforms a template and explicitly instantiate

--- simple_wavetable_synth/pmuse/waveform.cpp
+++ simple_wavetable_synth/pmuse/waveform.cpp
@@ -1,27 +1,32 @@
 #ifndef _WAVEFORM_H
 #include "waveform.hpp"
 
-pmuse::Waveform_i8::Waveform_i8(int32_t _length){
+using namespace::pmuse;
+
+template <typename T>
+Waveform<T>::Waveform(int32_t _length){
     length = _length;
-    vect = std::vector<int8_t> ( _length);
+    vect = std::vector<T> ( _length);
 }
 
-
-pmuse::Waveform_i8::Waveform_i8(int32_t _length, int8_t arr[]){
+template <typename T>
+Waveform<T>::Waveform(int32_t _length, T arr[]){
     length = _length;
-    vect = std::vector<int8_t> ( _length);
+    vect = std::vector<T> ( _length);
     for (int8_t i = 0; i<length; i++){
         vect[i] = arr[i];
     }
 }
 
-pmuse::Waveform_i8::~Waveform_i8(){}
+template <typename T>
+Waveform<T>::~Waveform(){}
 
 /** By default, check that idx doesn't excede the size of the array.
  This check can be removed by setting attribute ```check_bounds``` to
  ```false```
  */
-int8_t& pmuse::Waveform_i8::operator[](int32_t idx){
+template <typename T>
+T& Waveform<T>::operator[](int32_t idx){
     if (check_bounds) {
         return vect[idx % length];
     } else {
@@ -29,8 +34,11 @@ int8_t& pmuse::Waveform_i8::operator[](int32_t idx){
     }
 }
 
-int8_t pmuse::Waveform_i8::get_length(){
+template <typename T>
+int32_t Waveform<T>::get_length(){
     return length;
 }
+
+template class Waveform<int8_t>;
 
 #endif
--- simple_wavetable_synth/pmuse/waveform.hpp
+++ simple_wavetable_synth/pmuse/waveform.hpp
@@ -8,30 +8,28 @@
 
 namespace pmuse{
 
-// Ideally Waveform would be a template, but that involves defining in the header.
-// Currently only have one implementation, if this changes rethink.
-
-/** Contains the data for a single waveform. Data is stored as 8-bit signed integers.
+/** Class template for storing and accessing the data in a single waveform.
 
  The length can be passed at runtime
  and the data can be changed at runtime. Can access he ```i```th
  sample by just calling ```wav[i]``` where ```wav``` is a ```Waveform<T>``` instance.
  */
-class Waveform_i8
+template <typename T>
+class Waveform
 {
 private:
     int32_t length;
-    std::vector<int8_t> vect;
+    std::vector<T> vect;
     bool check_bounds = true;
 
 public:
-    Waveform_i8(int32_t _length);
-    Waveform_i8(int32_t _length, int8_t arr[]);
-    ~Waveform_i8();
+    Waveform(int32_t _length);
+    Waveform(int32_t _length, T arr[]);
+    ~Waveform();
 
-    int8_t &operator[](int32_t idx);
+    T &operator[](int32_t idx);
 
-    int8_t get_length();
+    int32_t get_length();
     bool set_check_bounds(bool _check_bounds);
     bool get_check_bounds();
 };
@@ -39,9 +37,9 @@ public:
 /**8-bit waveform.
  Contains the data for a single waveform. The length can be passed at runtime
  and the data can be changed at runtime. Can access he ```i```th
- sample by just calling ```wav[i]``` where ```wav``` is a ```Waveform<T>``` instance.
+ sample by just calling ```wav[i]``` where ```wav``` is an instance.
 */
-// typedef Waveform<int8_t> Waveform8;
+typedef Waveform<int8_t> Waveform_i8;
 }
 
 #endif
